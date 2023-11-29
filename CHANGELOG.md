2e53fba4ce198f888a977653e511634f33b4c972
William Redding <williamdredding@gmail.com>
2023-11-29 11:27:11 -0800

    Convert co CMake build system

--- CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# Initial Release
-* Fragment shader loading
-* 3 default uniforms - iMouse, iResolution, iTime
-* ImGUI control menu with for uniform control
-
-# Update 2 - 25/06/2023
-* Changed default uniforms iMouse and iResolution to float vec2 from int vec2 to fix bug
---
+++ src/core/Application.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <cmath>
+#include <gl.h>
+#include <GLFW/glfw3.h>
+#include <shader/ShaderManager.hpp>
+
+class Application
+{
+private:
+	static std::unique_ptr<Application> s_Instance;
+	static GLFWwindow* p_WallpaperWindow;
+	static GLFWwindow* p_ImGUIWindow;
+
+	ShaderManager* shaderManager = nullptr;
+
+	wchar_t* wallpaper_dir;
+	wchar_t* GetWallpaper();
+	void SetWallpaper(const wchar_t* path);
+	
+	void CheckImGUIButtons();
+	bool m_isLoadShaderButtonPressed = false;
+
+public:
+	Application();
+
+	void Run();
+	void ResetWallpaper();
+	static std::unique_ptr<Application>& GetInstance();
+	void SendDefaultUniforms();
+};
+
